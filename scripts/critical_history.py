eb4f6213448953a289495fb6b6b31e9b0aea978e
Julie Fortuna <julie.fortuna@gmail.com>
2013-11-14 02:47:12 -0800

    plots for degree distribution, lots of stuff commented out while I was trying to figure out which node was the satoshidice one

--- scripts/critical_history.py
+++ scripts/critical_history.py
@@ -2,7 +2,8 @@ import graphgen
 import graphtools
 import networkx as nx
 from networkx.algorithms import simple_paths
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import operator
 
 # public trading started at the end of
 # April, 2010. This gives a slice beforehand
@@ -14,12 +15,11 @@ _START_PUBLIC_TRADING = 20100401*graphgen._HMS
 _END_PUBLIC_TRADING = 20100531*graphgen._HMS
 
 # Announcement date was April 24, 2012
-# Start a week previously
-_START_SATOSHI_DICE = 2012042*graphgen._HMS
+# Start 7 days previously
+_START_SATOSHI_DICE = 20120417*graphgen._HMS
 
-# Ending the search after a month of
-# launch date
-_END_SATOSHI_DICE = 20120427*graphgen._HMS
+# Ending the search 7 days after launch
+_END_SATOSHI_DICE = 20120501*graphgen._HMS
 
 # official launch date so nodes
 # can be separated to before/after
@@ -47,11 +47,55 @@ def analyze_satoshi_dice():
     Analyzes the snippet of the bitcoin network around the time that
     SatoshiDice was announced. There is a hard date for its launch.
     """
-    g_before = graphgen.get_graph_slice(_START_SATOSHI_DICE, _LAUNCH_SATOSHI_DICE-graphgen._HMS)
+    g_before = graphgen.get_graph_slice(_START_SATOSHI_DICE, _LAUNCH_SATOSHI_DICE)
     g_after = graphgen.get_graph_slice(_START_SATOSHI_DICE, _END_SATOSHI_DICE)
     before_lcc = graphtools.get_lcc_from_graph(g_before)
     after_lcc = graphtools.get_lcc_from_graph(g_after)
     print len(before_lcc), len(after_lcc)
+    '''
+    before_in = g_before.in_degree()
+    before_in_values = sorted(set(before_in.values()))
+    before_in_hist = [before_in.values().count(x)/float(len(g_before.nodes())) for x in before_in_values]
+    after_in = g_after.in_degree()
+    after_in_values = sorted(set(after_in.values()))
+    after_in_hist = [after_in.values().count(x)/float(len(g_after.nodes())) for x in after_in_values]
+
+    plt.loglog(before_in_values, before_in_hist, 'bo', label='In-degree before SatoshiDICE announced')
+    plt.loglog(after_in_values, after_in_hist, 'ro', label='In-degree after SatoshiDICE announced')
+    plt.legend(loc=1)
+    plt.xlabel('Degree')
+    plt.ylabel('Fraction of nodes')
+    plt.title('In-degree 1 week before and after SatoshiDICE announced')
+    plt.show()
+    
+    before_out = g_before.out_degree()
+    before_out_values = sorted(set(before_out.values()))
+    before_out_hist = [before_out.values().count(x)/float(len(g_before.nodes())) for x in before_out_values]
+    after_out = g_after.out_degree()
+    after_out_values = sorted(set(after_out.values()))
+    after_out_hist = [after_out.values().count(x)/float(len(g_after.nodes())) for x in after_out_values]
+
+    plt.loglog(before_out_values, before_out_hist, 'bo', label='Out-degree before SatoshiDICE announced')
+    plt.loglog(after_out_values, after_out_hist, 'ro', label='Out-degree after SatoshiDICE announced')
+    plt.legend(loc=1)
+    plt.xlabel('Degree')
+    plt.ylabel('Fraction of nodes')
+    plt.title('Out-degree 1 week before and after SatoshiDICE announced')
+    plt.show()
+    '''
+
+
+    #before_node_max_in = graphtools.get_node_max_in_degree(g_before)
+    #after_node_max_in = graphtools.get_node_max_in_degree(g_after)
+    #print before_node_max_in, after_node_max_in
+    #print sorted(g_before.in_degree().iteritems(), key=operator.itemgetter(1))
+    #print sorted(g_after.in_degree().iteritems(), key=operator.itemgetter(1))
+    #print g_after.edges('25', data=True)
+    #print nx.get_edge_attributes(g_after, 'transaction_key')
+    #print g_after.in_degree(after_node_max_in)
+    #new_nodes = set(g_after.in_degree())-set(g_before.in_degree())
+    #new_nodes_degrees = {k: g_after.in_degree()[k] for k in new_nodes}
+    #print sorted(new_nodes_degrees.iteritems(), key=operator.itemgetter(1))
     #TODO get lccs, nodes with largest in-degree, k-core possibly
 
 def analyze_silk_road():
