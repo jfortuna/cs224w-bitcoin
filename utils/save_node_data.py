1779f32cb9334f651385baed270e86fce7247639
Julie Fortuna <julie.fortuna@gmail.com>
2013-12-03 20:14:47 -0800

    Script to save node data to a csv in csv_data folder

--- utils/save_node_data.py
+++ utils/save_node_data.py
@@ -1,7 +1,10 @@
-
-
+import csv
 
 def save_node_map(mp, stamp):
-  """Saves data about the nodes to a csv. Note that this assumes
-     the key is a node. Stamp is the name of the csv.
-  """
+    """Saves data about the nodes to a csv. Note that this assumes
+    the key is a node. Stamp is the name of the csv.
+    """
+    with open('../csv_data/'+stamp, 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        for key, value in mp.items():
+            writer.writerow([key, value])
