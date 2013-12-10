1727238e3615b73d39055ddb40070c47cdeb5158
Julie Fortuna <julie.fortuna@gmail.com>
2013-12-10 13:27:00 -0800

    added forest fire

--- scripts/data_over_time_nonbtc.py
+++ scripts/data_over_time_nonbtc.py
@@ -7,8 +7,9 @@ import networkx as nx
 import graphgen
 import graphtools
 import tags_over_time
+import forest_fire as ff
 from networkx.algorithms import *
-
+'''
 _HMS = 1000000
 slices = graphgen.generate_weighted_time_slices()
 i = 0
@@ -64,9 +65,44 @@ utils.save_lists(dates, avg_clust_p, stamp='pfg_avg_clust')
 utils.save_lists(dates, lccs_p, stamp='pfg_lccs')
 # plot single values over time
 # Gini coefficient (way to get single number from distribution)
+'''
 
 
 
+_HMS = graphgen._HMS
+slices = graphgen.generate_weighted_time_slices()
+i = 0
+dates = []
+avg_clust = []
+lccs = []
+largest_scc = []
 
+p = 0.37
+p_b = 0.32
+r = p / p_b
 
+for start, end in slices:
+    g = graphgen.get_graph_slice(start * _HMS, end * _HMS)
+    if len(g) == 0:
+        continue
+    ff = ff.forest_fire(g.number_of_nodes(), p, r)
+    stamp = str(start) + '_' + str(end)
+    
+    tags_over_time.user_transaction_frequency(ff, 'ff_' + stamp)
+    tags_over_time.user_buy_frequency(ff, 'ff_' + stamp)
+    tags_over_time.user_sell_frequency(ff, 'ff_' + stamp)
+  
+    undir_ff = nx.Graph(ff.copy())
+    avg_clust.append(nx.average_clustering(undir_ff))
+    lccs.append(len(graphtools.get_lcc_from_graph(ff)))
+    largest_scc.append(len(graphtools.get_sccs_from_graph(ff)[0]))
+    
+    dates.append(start) # hack for now to save time...
+    
+    print 'finished %s tag over time' % i
+    i += 1
+
+utils.save_lists(dates, avg_clust, stamp='ff_avg_clust')
+utils.save_lists(dates, lccs, stamp='ff_lccs')
+utils.save_lists(dates, largest_scc, stamp='ff_largest_scc')
 
